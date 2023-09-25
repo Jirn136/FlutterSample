07b5497790e18c6ad2739ea08cc8d6ec23bf6e19
JanakiRaman <janakiraman.b@contusin>
2023-09-25 17:06:02 +0530

    Removed unwanted provider widget

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sample/database/database.dart';
 import 'package:flutter_sample/screens/list_screen.dart';
 import 'package:flutter_sample/screens/login.dart';
-import 'package:flutter_sample/screens/tenant_data.dart';
 import 'package:flutter_sample/utils/constants.dart';
 import 'package:flutter_sample/utils/shared_preferences.dart';
-import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
@@ -14,10 +11,6 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     PreferenceUtils pref = PreferenceUtils();
     bool isChecked = pref.getBoolean(Constants.isChecked);
-    return Provider(
-        create: (context) => AppDatabase(),
-        child:
-        isChecked ? const ListScreen() : const Login()
-    );
+    return isChecked ? const ListScreen() : const Login();
   }
 }
